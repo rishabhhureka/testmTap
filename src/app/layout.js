aae993719ebe64972a6f5d88b254da25139981a8
rishabhhureka <rishabh@hurekatek.com>
2024-05-21 13:16:16 +0530

    added schema

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: `{
+            __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "Organization",
               "name": "mTap",
@@ -33,8 +33,13 @@ export default function RootLayout({ children }) {
                 "https://www.facebook.com/mTap.Magic.Marketing",
                 "https://www.instagram.com/mtappro/",
                 "https://www.tiktok.com/@mtapsocial"
-              ]
-            }`,
+              ],
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+1-800-555-5555",
+                "contactType": "Customer Service"
+              }
+            })
           }}
         />
       </head>
