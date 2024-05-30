411da113d19892666540041cd54b624e0be4fb82
rishabhhureka <rishabh@hurekatek.com>
2024-05-30 18:46:57 +0530

    added pro page and commented login for now

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import Router from 'next/router';
+// import axios from 'axios';
+// import Router from 'next/router';
 
-export async function renewToken() {
-  try {
-    const response = await axios.get('/api/token');
-    if (response.status === 200) {
-      return response.data.accessToken;
-    }
-  } catch (error) {
-    console.error('Failed to renew token', error);
-    Router.push('/api/auth/login');
-  }
-}
+// export async function renewToken() {
+//   try {
+//     const response = await axios.get('/api/token');
+//     if (response.status === 200) {
+//       return response.data.accessToken;
+//     }
+//   } catch (error) {
+//     console.error('Failed to renew token', error);
+//     Router.push('/api/auth/login');
+//   }
+// }
