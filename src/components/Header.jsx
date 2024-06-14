e13eb3b63b9569d526b2d4cc5097f6853a951e03
rishabhhureka <163499927+rishabhhureka@users.noreply.github.com>
2024-06-14 19:09:58 +0530

    Revert "added listing page"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import mtapLogo from "../Img/mtapLogo.png";
 import menuIcon from "../Img/menu.png";
 import closeIcon from "../Img/close.png";
+import { Menu, MenuItem, Button } from '@mui/material';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 // import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
@@ -63,41 +64,8 @@ const Header = () => {
               onClick={toggleMenu}
             />
           </button>
-          <li  className="dropdown">
-            <Link href="/shop-nfc" aria-current="page" className="nav-link">Shop NFC</Link>
-            <div className="dropdown-content">
-       <div className="emptyDropdown"></div>
-     
-      
-      <div className="maindropdown-content">
-      <p className="menu-item">
-     <Link href="/shop-nfc/custom-digital-business-cards">mTap Custom Cards</Link>
-     </p>
-        <p className="menu-item">
-        <Link href="/shop-nfc/google-review-cards">Google Review Cards</Link>
-       
-        </p>
-     <p className="menu-item">
-     <Link href="/shop-nfc/metal-cards">NFC Black Metal Card</Link>
-     </p>
-     <p className="menu-item">
-     <Link href="/shop-nfc/mtap-key-fobs">mTap Key Fobs</Link>
-     </p>
-     <p className="menu-item">
-     <Link href="/shop-nfc/mtap-retail-solutions">mTap Retail Solutions</Link>
-     </p>
-     <p className="menu-item">
-     <Link href="/shop-nfc/mtap-business-cards">mTap Business Cards</Link>
-     </p>
-     <p className="menu-item">
-     <Link href="/shop-nfc/mtap-collections">mTap Collections</Link>
-     </p>
-     <p className="menu-item">
-     <Link href="/shop-nfc/mtap-stickers">mTap Stickers</Link>
-     </p>
-               
-     </div>
-      </div>
+          <li>
+            <Link href="/product" aria-current="page" className="nav-link">Shop NFC</Link>
           </li>
        
           <li>
